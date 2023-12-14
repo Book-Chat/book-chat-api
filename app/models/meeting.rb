d05c2f7dbdca5c0fa36bc63caaf726aa1722a48b
K.D. Hubbard <hubbard.kd@gmail.com>
2023-12-14 10:29:01 -0700

    meeting associations

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,3 +1,5 @@
 class Meeting < ApplicationRecord
   belongs_to :book_club
+  has_one :book_club_book
+  has_one :book, through: :book_club_book
 end
--- spec/models/meeting_spec.rb
+++ spec/models/meeting_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Meeting, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do 
+    it { should belong_to :book_club }
+    it { should have_one :book_club_book }
+    it { should have_one(:book).through(:book_club_book)}
+  end
 end
