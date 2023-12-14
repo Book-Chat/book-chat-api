d8ee8d5d4b60d1d0b9644937b84cf1710a063434
K.D. Hubbard <hubbard.kd@gmail.com>
2023-12-14 09:36:29 -0700

    add book club associations

--- spec/models/book_club_spec.rb
+++ spec/models/book_club_spec.rb
@@ -5,4 +5,14 @@ RSpec.describe BookClub, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :status }
   end
+
+  describe "associations" do 
+    it { should have_many :book_club_subjects }
+    it { should have_many(:subjects).through(:book_club_subjects)}
+    it { should have_many :meetings }
+    it { should have_many :book_club_books }
+    it { should have_many(:books).through(:book_club_books)}
+    it { should have_many :book_club_users }
+    it { should have_many(:users).through(:book_club_users)}
+  end
 end
