dfc2d9c81d8aa024d3068468a42d21a35988bfff
K.D. Hubbard <hubbard.kd@gmail.com>
2023-12-14 10:48:09 -0700

    add book club book associations

--- spec/models/book_club_book_spec.rb
+++ spec/models/book_club_book_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe BookClubBook, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do 
+    it { should belong_to :book }
+    it { should belong_to :book_club }
+    it { should belong_to :meeting }
+  end
 end
+
