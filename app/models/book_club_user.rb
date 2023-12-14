49bf6e2d00b40a271c39377c3913b7782d25ae1e
K.D. Hubbard <hubbard.kd@gmail.com>
2023-12-14 10:23:27 -0700

    book club users validations and associations

--- app/models/book_club_user.rb
+++ app/models/book_club_user.rb
@@ -1,4 +1,7 @@
 class BookClubUser < ApplicationRecord
   belongs_to :user
   belongs_to :book_club
+
+  validates :role, presence: true
+  enum :role, { member: 0, owner: 1, collaborator: 2 }
 end
--- spec/models/book_club_user_spec.rb
+++ spec/models/book_club_user_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe BookClubUser, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "associations" do 
+    it { should belong_to :user }
+    it { should belong_to :book_club }
+  end
+
+  describe "validations" do 
+    it { should validate_presence_of :role }
+  end
 end
