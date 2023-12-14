c3bcc443abca3aec5f277a7cd4add426a461a2e8
K.D. Hubbard <hubbard.kd@gmail.com>
2023-12-14 09:46:36 -0700

    add subject associations

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,7 @@ class Subject < ApplicationRecord
   validates :type, presence: true
   validates :name, presence: true
   validates :description, presence: true
+
+  has_many :book_club_subjects
+  has_many :book_clubs, through: :book_club_subjects
 end
--- spec/models/subject_spec.rb
+++ spec/models/subject_spec.rb
@@ -6,4 +6,9 @@ RSpec.describe Subject, type: :model do
     it { should validate_presence_of :name }
     it { should validate_presence_of :description }
   end
+
+  describe "associations" do 
+    it { should have_many :book_club_subjects }
+    it { should have_many(:book_clubs).through(:book_club_subjects) }
+  end
 end
