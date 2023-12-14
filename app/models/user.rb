d2470da78c0cefb7290025ba6d9a3e038ea3edbb
K.D. Hubbard <hubbard.kd@gmail.com>
2023-12-14 10:09:53 -0700

    add user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username, uniqueness: true, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true, confirmation: true
+  validates :password_confirmation, presence: true
+  validates :password_digest, presence: true
+
+  has_secure_password
 end
